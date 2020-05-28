e1fe387395afccf1bea125b82219d0cad7eb331c
Stefan Piperov <piperov@cern.ch>
2020-05-28 18:30:08 -0400

    First import

---
+++ setup_proxy.sh
@@ -0,0 +1,8 @@
+voms-proxy-init -voms cms -rfc -valid 192:00
+export VOMS_PATH=$(echo $(voms-proxy-info | grep path) | sed 's/path.*: //')
+export VOMS_USERID=$(echo $(voms-proxy-info | grep path) | sed 's/.*p_u//')
+export VOMS_TRG=/home/$USER/x509up_u$VOMS_USERID
+rm -f $VOMS_TRG
+cp $VOMS_PATH $VOMS_TRG
+echo "Your proxy is copied here: "$VOMS_TRG
+export X509_USER_PROXY=$VOMS_TRG
---
+++ slurm_cluster_prep.py
@@ -0,0 +1,7 @@
+from dask_jobqueue import SLURMCluster
+from dask.distributed import Client 
+import pytest
+from coffea.processor.executor import dask_executor
+import dask
+from python.dimuon_processor import DimuonProcessor
+
